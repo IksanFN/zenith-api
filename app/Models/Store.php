9aed9f94d31fdad18ff097f86a8d60d53e27fc0c
Iksan Fauzi Nugraha <iksanfauzi727@gmail.com>
2025-12-15 14:04:22 +0700

    Model and Migration Store

---
+++ app/Models/Store.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\UUID;
+use Illuminate\Database\Eloquent\Model;
+
+class Store extends Model
+{
+    use UUID;
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'slug',
+        'logo',
+        'description',
+        'phone',
+        'email',
+        'address_id',
+        'address',
+        'city',
+        'country',
+        'latitude',
+        'longitude',
+        'is_active',
+        'is_verified',
+    ];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+        'is_verified' => 'boolean',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/migrations/2025_12_15_063620_create_stores_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stores', function (Blueprint $table) {
+            $table->uuid()->primary();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->string('logo')->nullable();
+            $table->text('description')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('email')->nullable();
+            $table->integer('address_id')->nullable();
+            $table->text('address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('country')->nullable();
+            $table->string('latitude')->nullable();
+            $table->string('longitude')->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->boolean('is_verified')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stores');
+    }
+};
